520e70aed653b38bdc245ec35b15b66498d92c11
pkeene17 <pkeene17@gmail.com>
2021-03-03 18:02:43 -0600

    new sesh names

--- getSessionNames.m
+++ getSessionNames.m
@@ -3,14 +3,14 @@
 %ouput - string subjects - cell array of subject identifier strings
 %      - int nsubj - number of subjects
 
-function [subjects,nsubj] = getSubjectNames(projDir)
+function [subjects,nsubj] = getSessionNames(projDir)
 
-dinfo = dir(strcat(projDir,'/sorted_data/neural/','nicklab*.*'));
+dinfo = dir(strcat(projDir,'/sorted_data/beh/stimuli/','nicklab*.*'));
 names = {dinfo.name};
-nsubj = length(names)/2;
+nsubj = length(names)/3;
 subjects = {};
 
 for isubj = 1:nsubj
-    subj = extractBefore(names{((isubj-1)*2)+1},'spikes');
+    subj = extractBefore(names{((isubj-1)*2)+1},'trials');
     subjects{isubj} = subj;
 end
