6fc70bc64dddf46f0d70b6a4dbd56eedf3e5a8b1
pkeene17 <pkeene17@gmail.com>
2021-03-03 19:36:42 -0600

    changes to load data

--- loadData.m
+++ loadData.m
@@ -1,11 +1,23 @@
 %% script to load in the files for the final project
-%output - beh - behavioral data for the project. Contains fields
+%output - beh - behavioral data for the project. Contains fields:
+%               goCue - time audio cue sounded (ms)
+%               respTimes - time a response was logged (ms)
+%               stimTimes - time the stimulus appeared (ms)
+%               contrastLeft - contrast level of stimulus in left field
+%               contrastRight - contrast level of stimulus in right field
+%               resp - which response was logged (left, right, none)
+%               wheelMove - direction of wheel movement
+%               wheelPos - wheel position during trial
+%               wheelTime - timestamps for wheel pos
+%       - eye - eye tracking data for subject. Contains fields:
+%               area - area of pupil (au)
+%               xypos - xy position of gaze (au)
+%               times - time for eye position and area (ms)
 
-function [beh,eye,neural] = loadData(subjects,nsubj,projDir)
+function [beh,eye] = loadData(subjects,nsubj,projDir)
 
 beh = struct;
 eye = struct;
-neural = struct;
 
 for isubj = 1:nsubj
     beh(isubj).goCue = readNPY([projDir,'/sorted_data/beh/times/',subjects{isubj},'trials.goCue_times.npy']);
@@ -16,9 +28,8 @@ for isubj = 1:nsubj
     beh(isubj).resp = readNPY([projDir,'/sorted_data/beh/resp/',subjects{isubj},'trials.response_choice.npy']);
     beh(isubj).wheelMove = readNPY([projDir,'/sorted_data/beh/resp/',subjects{isubj},'wheelMoves.type.npy']);
     beh(isubj).wheelPos = readNPY([projDir,'/sorted_data/beh/resp/',subjects{isubj},'wheel.position.npy']);
+    beh(isubj).wheelTime = readNPY([projDir,'/sorted_data/beh/times/',subjects{isubj},'wheel.timestamps.npy']);
     eye(isubj).area = readNPY([projDir,'/sorted_data/eye/',subjects{isubj},'eye.area.npy']);
     eye(isubj).xypos = readNPY([projDir,'/sorted_data/eye/',subjects{isubj},'eye.xypos.npy']);
     eye(isubj).times = readNPY([projDir,'/sorted_data/eye/',subjects{isubj},'eye.timestamps.npy']);
-    neural(isubj).times = readNPY([projDir,'/sorted_data/neural/',subjects{isubj},'spikes.times.npy']);
-    neural(isubj).spikes = readNPY([projDir,'/sorted_data/neural/',subjects{isubj},'spikes.clusters.npy']);
 end
