bfb871eb6f9e96a68f11db88c0f0944ec72505f9
Erica Lin <ericalin.135@gmail.com>
2021-03-03 18:51:34 -0800

    add ISI for diff in left/right stimuli

---
+++ visualize/isi_subject1_contrast.m
@@ -0,0 +1,74 @@
+function [] = isi_subject1_contrast(beh, spikes)
+%RASTER Provide ISI for subject 1's visual neurons
+%       -0.1 to 0.5 s relative to stimuli times to difference in left/right contrast 
+%       (high for difference of 1, medium for difference of 0.5,
+%       no contrast for difference of 0)
+subject1_beh = beh(1);
+subject1_spikes = spikes(1).VISpTimes;
+
+% VISp response to high difference (1) between left and right contrasts
+high_diff_trial_indices = find(abs(subject1_beh.contrastLeft - subject1_beh.contrastRight) == 1);
+num_high_diff_trials = length(high_diff_trial_indices);
+interspike_intervals_pooled = [];
+for cur_array_idx = 1:num_high_diff_trials
+    trial_index = high_diff_trial_indices(cur_array_idx);
+    stim_time = subject1_beh.stimTimes(trial_index);
+    spikes_s = subject1_spikes(subject1_spikes >= stim_time - 0.1 & subject1_spikes <= stim_time + 0.5);
+    spikes_s = spikes_s - stim_time; % Align data to stim_time
+    spikes_ms = spikes_s * 1000;
+    interspike_intervals_ms = diff(spikes_ms);
+    interspike_intervals_pooled = [interspike_intervals_pooled; interspike_intervals_ms];
+end
+
+interspike_intervals_pooled = interspike_intervals_pooled(interspike_intervals_pooled > 0);
+figure
+title('VISp neuron activity at -100ms to 500ms relative to high contrast stimuli time (for subject 1)','fontsize',18)
+xlabel('ISI time (ms)','FontSize',14)
+ylabel('Frequency (Normalized proportion)','FontSize',14)
+hold on 
+histogram(interspike_intervals_pooled,'Normalization','probability');   
+hold off
+
+% VISp response to medium difference (0.5) between left and right contrasts
+medium_diff_trial_indices = find(abs(subject1_beh.contrastLeft - subject1_beh.contrastRight) == 0.5);
+num_medium_diff_trials = length(medium_diff_trial_indices);
+interspike_intervals_pooled = [];
+for cur_array_idx = 1:num_medium_diff_trials
+    trial_index = medium_diff_trial_indices(cur_array_idx);
+    stim_time = subject1_beh.stimTimes(trial_index);
+    spikes_s = subject1_spikes(subject1_spikes >= stim_time - 0.1 & subject1_spikes <= stim_time + 0.5);
+    spikes_s = spikes_s - stim_time; % Align data to stim_time
+    spikes_ms = spikes_s * 1000;
+    interspike_intervals_ms = diff(spikes_ms);
+    interspike_intervals_pooled = [interspike_intervals_pooled; interspike_intervals_ms];
+end
+interspike_intervals_pooled = interspike_intervals_pooled(interspike_intervals_pooled > 0);
+figure
+title('VISp neuron activity at -100ms to 500ms relative to medium contrast stimuli time (for subject 1)','fontsize',18)
+xlabel('ISI time (ms)','FontSize',14)
+ylabel('Frequency (Normalized proportion)','FontSize',14)
+hold on 
+histogram(interspike_intervals_pooled,'Normalization','probability');   
+hold off
+
+% VISp response to no difference (0) between left and right contrasts
+no_diff_trial_indices = find(abs(subject1_beh.contrastLeft - subject1_beh.contrastRight) == 0);
+num_no_diff_trials = length(no_diff_trial_indices);
+interspike_intervals_pooled = [];
+for cur_array_idx = 1:num_no_diff_trials
+    trial_index = no_diff_trial_indices(cur_array_idx);
+    stim_time = subject1_beh.stimTimes(trial_index);
+    spikes_s = subject1_spikes(subject1_spikes >= stim_time - 0.1 & subject1_spikes <= stim_time + 0.5);
+    spikes_s = spikes_s - stim_time; % Align data to stim_time
+    spikes_ms = spikes_s * 1000;
+    interspike_intervals_ms = diff(spikes_ms);
+    interspike_intervals_pooled = [interspike_intervals_pooled; interspike_intervals_ms];
+end
+interspike_intervals_pooled = interspike_intervals_pooled(interspike_intervals_pooled > 0);
+figure
+title('VISp neuron activity at -100ms to 500ms relative to no contrast stimuli time (for subject 1)','fontsize',18)
+xlabel('ISI time (ms)','FontSize',14)
+ylabel('Frequency (Normalized proportion)','FontSize',14)
+hold on 
+histogram(interspike_intervals_pooled,'Normalization','probability');   
+hold off
--- visualize/raster_subject1_directional.m
+++ visualize/raster_subject1_directional.m
@@ -1,12 +1,13 @@
-function [] = raster_subject1(beh, spikes)
+function [] = raster_subject1_directional(beh, spikes)
 %RASTER Provide raster for subject 1's motor neurons
-%       -0.5 to 1 s relative to response times to left stimuli
+%       -0.5 to 1 s relative to response times to direction (left, right,
+%       neutral stimuli)
 subject1_beh = beh(1);
 subject1_spikes = spikes(1).MOsTimes;
 
 % MOs response to left stimuli
 figure
-title('MOs neuron activity at response time for left stimuli (for subject 1)','fontsize',22)
+title('MOs neuron activity at response time for left stimuli (for subject 1)','fontsize',18)
 xlabel('Spike time (ms)','FontSize',14)
 ylabel('Trial','FontSize',14)
 hold on 
@@ -30,50 +31,50 @@ hold off
 
 % MOs response to right stimuli
 figure
-title('MOs neuron activity at response time for right stimuli (for subject 1)','fontsize',22)
+title('MOs neuron activity at response time for right stimuli (for subject 1)','fontsize',18)
 xlabel('Spike time (ms)','FontSize',14)
 ylabel('Trial','FontSize',14)
 hold on 
 
-left_trial_indices = find(subject1_beh.contrastLeft < subject1_beh.contrastRight);
-num_left_trials = length(left_trial_indices);
+right_trial_indices = find(subject1_beh.contrastLeft < subject1_beh.contrastRight);
+num_right_trials = length(right_trial_indices);
 
-for cur_array_idx = 1:num_left_trials
-    trial_index = left_trial_indices(cur_array_idx);
+for cur_array_idx = 1:num_right_trials
+    trial_index = right_trial_indices(cur_array_idx);
     resp_time = subject1_beh.respTimes(trial_index);
     responses_s = subject1_spikes(subject1_spikes >= resp_time - 0.5 & subject1_spikes <= resp_time + 1);
     responses_s = responses_s - resp_time; % Align data to resp_time
     responses_ms = responses_s * 1000;
     for response = responses_ms
-        y  = num_left_trials - cur_array_idx;
+        y  = num_right_trials - cur_array_idx;
         line([response response],[y, y + 0.8],'color','black','LineWidth',1)
     end
 end
-set(gca,'YTick',(1:8:num_left_trials)-0.5,'YTickLabel',num_left_trials:-8:1)
+set(gca,'YTick',(1:8:num_right_trials)-0.5,'YTickLabel',num_right_trials:-8:1)
 hold off
 
 % MOs response to neutral stimuli
 figure
-title('MOs neuron activity at respoonse time for neutral stimuli (for subject 1)','fontsize',22)
+title('MOs neuron activity at respoonse time for neutral stimuli (for subject 1)','fontsize',18)
 xlabel('Spike time (ms)','FontSize',14)
 ylabel('Trial','FontSize',14)
 hold on 
 
-left_trial_indices = find(subject1_beh.contrastLeft == subject1_beh.contrastRight);
-num_left_trials = length(left_trial_indices);
+neutral_trial_indices = find(subject1_beh.contrastLeft == subject1_beh.contrastRight);
+num_neutral_trials = length(neutral_trial_indices);
 
-for cur_array_idx = 1:num_left_trials
-    trial_index = left_trial_indices(cur_array_idx);
+for cur_array_idx = 1:num_neutral_trials
+    trial_index = neutral_trial_indices(cur_array_idx);
     resp_time = subject1_beh.respTimes(trial_index);
     responses_s = subject1_spikes(subject1_spikes >= resp_time - 0.5 & subject1_spikes <= resp_time + 1);
     responses_s = responses_s - resp_time; % Align data to resp_time
     responses_ms = responses_s * 1000;
     for response = responses_ms
-        y  = num_left_trials - cur_array_idx;
+        y  = num_neutral_trials - cur_array_idx;
         line([response response],[y, y + 0.8],'color','black','LineWidth',1)
     end
 end
-set(gca,'YTick',(1:8:num_left_trials)-0.5,'YTickLabel',num_left_trials:-8:1)
+set(gca,'YTick',(1:8:num_neutral_trials)-0.5,'YTickLabel',num_neutral_trials:-8:1)
 hold off
 
 end
